93387b66d2cdcec9ef2d13647f1fc20bec1dadd7
ftxdante <blog.anutrof@gmail.com>
2021-08-23 21:01:26 -0300

    Comparador de datas

--- exercicios/exercicio-comparador-de-datas/src/ComparadorData.java
+++ exercicios/exercicio-comparador-de-datas/src/ComparadorData.java
@@ -1,9 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class ComparadorData {
     //Atributos
     private int dia;
     private int mes;
     private int ano;
 
+    public ComparadorData(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
     //Métodos
     //Teste1: Se o ano for maior o algoritmo escreve  1.
     //Teste2: Se o ano e o mês forem maior o algoritmo escreve  1.
@@ -14,36 +21,20 @@ public class ComparadorData {
     //Confirmação de Menoridade: Se for diferente disso significa que são menores então o algoritmo escreve -1
 
     //Setters
-    public void setDia(int dia){
-        if(dia > 31 || dia < 1){
-            System.out.println("Erro: Dia inválido");
-        }else{
-            this.dia = dia;
-        }
+
+    public int comparaDatas(ComparadorData data){
+       if(data.ano > this.ano || data.mes > this.mes || data.dia > this.dia){
+           return 1;
+       }else if(data.ano < this.ano || data.mes < this.mes || data.dia < this.dia){
+           return -1;
+       }else{
+           return 0;
+       }
     }
 
-    public void setMes(int mes){
-        if(mes >12 || mes <1){
-            System.out.println("Erro: Mês inválido");
-        }else{
-            this.mes = mes;
-        }
+    public int frasePadrao(int n){
+        return n;
     }
 
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
 
-    //Getters
-    public int getDia() {
-        return dia;
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public int getAno() {
-        return ano;
-    }
 }
--- exercicios/exercicio-comparador-de-datas/src/Main.java
+++ exercicios/exercicio-comparador-de-datas/src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        ComparadorData teste1 = new ComparadorData();
-        teste1.setAno(2021);
-        teste1.setDia(16);
-        teste1.setMes(8);
-
-        System.out.println(teste1.comparaDatas(17, 8, 2021));
+        ComparadorData teste1 = new ComparadorData(17, 8,2021);
+        ComparadorData teste2 = new ComparadorData(17,8,2021);
+        System.out.println(teste1.comparaDatas(teste2));
 
     }
 }
